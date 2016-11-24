bc4a47b738e90ed70af2fe491be80cbf895c3ba7
Sven Eberhardt <sven2@brown.edu>
2016-11-24 18:40:10 -0500

    Route CNN /guess through node

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var Server = require('./server.js');
 var session = require('express-session');
 var PgStore = require('connect-pg-simple')(session);
 var yargs = require('yargs').usage('Usage: [-p or --port=<port to run server on>]');
+var request = require('request');
 
 var argv = yargs.argv;
 var port = argv.port || argv.p || 8080;
@@ -28,10 +29,18 @@ var pgPassword = 'serrelab';
 
 const util = require('util');
 
+var guess_server = 'http://localhost:7777/guess';
+
 app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 app.use('/style',  express.static(__dirname + '/style'));
 app.use('/script',  express.static(__dirname + '/script'));
 app.use('/web_content', express.static(__dirname + '/web_content'));
+
+app.post('/guess', function(req,res) {
+    var x = request(guess_server);
+    req.pipe(x);
+    x.pipe(res);
+});
 
 // use this for storing the hashed password in the session
 //app.use(require('cookie-parser'));
